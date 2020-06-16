835dede92d93f78f352b85b97159012012a2ee46
justin_james <jjames11799@gmail.com>
2020-06-16 16:51:04 -0400

    Puppy lab done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,44 @@ class ApplicationController < Sinatra::Base
         erb :welcome
       end
 
-   
+      get '/puppies' do 
+        @puppies = Puppy.all
+        # binding.pry
+        erb :index
+      end
+
+      get '/puppies/new' do 
+        erb :new
+      end
+
+      get '/puppies/:id' do
+        @puppy = find_puppy(params)
+        erb :show 
+      end
+
+      post '/puppies' do 
+        puppy = Puppy.create(params)
+        redirect "puppies/#{puppy.id}"
+      end
+
+      get '/puppies/:id/edit' do 
+        @puppy = find_puppy(params)
+        erb :edit
+      end
+
+      patch '/puppies/:id' do
+        puppy = find_puppy(params)
+        puppy.update(name: params[:name], breed: params[:year])
+        redirect "/puppies/#{puppy.id}"
+      end
+
+      delete '/puppies/:id' do 
+        puppy = find_puppy(params)
+        puppy.destroy
+        redirect '/puppies'
+      end
+
+      def find_puppy(params)
+          Puppy.find(params[:id])
+      end
 end
